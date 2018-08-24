cf11ea271f43e382ebe85efb30728616a088fab7
JeanJNAP <jeanjnap@gmail.com>
2018-08-24 11:32:48 -0300

    Customised Exception Trait
    https://www.udemy.com/laravel-e-commerce-restful-api/learn/v4/t/lecture/9251060?start=0

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -3,13 +3,13 @@
 namespace App\Exceptions;
 
 use Exception;
-use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 class Handler extends ExceptionHandler
 {
+    use ExceptionTrait;
+    
     /**
      * A list of the exception types that are not reported.
      *
@@ -47,19 +47,11 @@ class Handler extends ExceptionHandler
      * @param  \Exception  $exception
      * @return \Illuminate\Http\Response
      */
+
     public function render($request, Exception $exception)
     {
         if ($request->expectsJson()) {
-            if ($exception instanceof ModelNotFoundException) {
-                return response()->json([
-                    'errors' => 'Product Model not found',
-                ], Response::HTTP_NOT_FOUND);
-            }
-            if ($exception instanceof NotFoundHttpException) {
-                return response()->json([
-                    'errors' => 'Incorect route',
-                ], Response::HTTP_NOT_FOUND);
-            }
+            return $this->apiException($request, $exception);
         }
 
         return parent::render($request, $exception);
