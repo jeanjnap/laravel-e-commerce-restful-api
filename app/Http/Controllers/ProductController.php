388cf2a8889bd60248198e0786a929316da0e570
JeanJNAP <jeanjnap@gmail.com>
2018-08-24 10:41:49 -0300

    Put Product

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -90,7 +90,14 @@ class ProductController extends Controller
      */
     public function update(Request $request, Product $product)
     {
-        //
+        $request['detail'] = $request->description;
+        unset($request['description']);
+        
+        $product->update($request->all());
+
+        return response([
+            'data' => ($product)
+        ],Response::HTTP_CREATED);
     }
 
     /**
--- app/Model/Product.php
+++ app/Model/Product.php
@@ -7,6 +7,10 @@ use App\Model\Review;
 
 class Product extends Model
 {
+    protected $fillable = [
+		'name','detail','stock','price','discount'
+    ];
+    
     public function reviews()
     {
         return $this->hasMany(Review::class);
