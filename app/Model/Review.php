d788cb44f60a330e6dd8b9871822ce5a891883f0
JeanJNAP <jeanjnap@gmail.com>
2018-08-27 11:29:27 -0300

    Review Created
    https://www.udemy.com/laravel-e-commerce-restful-api/learn/v4/t/lecture/9251064?start=0

--- app/Model/Review.php
+++ app/Model/Review.php
@@ -2,11 +2,15 @@
 
 namespace App\Model;
 
-use Illuminate\Database\Eloquent\Model;
 use App\Model\Product;
+use Illuminate\Database\Eloquent\Model;
 
 class Review extends Model
 {
+    protected $fillable = [
+        'star', 'customer', 'review',
+    ];
+
     public function product()
     {
         return $this->belongsTo(Product::class);
